b781a5d85110b1ff610716da661205e2477e6f0b
bala-amypo <bala@amypo.in>
2025-12-17 08:36:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -1,4 +1,10 @@
 package.com.example.demo.model;
 public class Location{
+    private Long id;
+    private String locationName;
+    private String description;
+    private String region;
+    private LocalDateTime createdAt;
+    
     
 }
--- src/main/java/com/example/demo/entity/Sensor.java
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -1,4 +1,9 @@
 package.com.example.demo.model;
 public class Sensor{
+    private Long id;
+    private String locationName;
+    private String description;
+    private String region;
+    private LocalDateTime createdAt;
     
 }
