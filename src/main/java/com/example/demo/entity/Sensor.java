7521702dcdc635387ed92f6501d759a9420ed852
bala-amypo <bala@amypo.in>
2025-12-17 08:56:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/Sensor.java
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -1,8 +1,8 @@
 package.com.example.demo.model;
 public class Sensor{
     private Long id;
-    private String locatio;
-    private String description;
+    private String sensorCode;
+    private String sensor;
     private String region;
     private LocalDateTime createdAt;
     
