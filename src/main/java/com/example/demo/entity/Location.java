3a8ae4f6cca8b5d9e1fca8d9631337f4d685bb50
bala-amypo <bala@amypo.in>
2025-12-17 08:41:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -6,5 +6,22 @@ public class Location{
     private String region;
     private LocalDateTime createdAt;
     
+    public Long getId(){
+        return id;
+    }
+    public void setId(long id){
+        this.id=id;
+    }
+    public String  getLocationName(){
+        return locationName;
+    }
+
+    public void setLocationName(String locationName){
+        this.locationName=locationName;
+    }
+    public String getDescription(){
+        return description;
+        
+    }
     
 }
