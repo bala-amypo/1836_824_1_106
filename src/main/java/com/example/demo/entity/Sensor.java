2ff348ea34a82e135675f52901325fa281621491
bala-amypo <bala@amypo.in>
2025-12-17 09:06:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/Sensor.java
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -2,8 +2,61 @@ package.com.example.demo.model;
 public class Sensor{
     private Long id;
     private String sensorCode;
-    private String sensor;
-    private String region;
-    private LocalDateTime createdAt;
+    private String sensorType;
+    private String location;
+    private LocalDateTime installedAt;
+    private Boolean isActive;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     
+    public String getSensorCode() {
+        return sensorCode;
+    }
+
+    public void setSensorCode(String sensorCode) {
+        this.sensorCode = sensorCode;
+    }
+
+    
+    public String getSensorType() {
+        return sensorType;
+    }
+
+    public void setSensorType(String sensorType) {
+        this.sensorType = sensorType;
+    }
+
+    
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    
+    public LocalDateTime getInstalledAt() {
+        return installedAt;
+    }
+
+    public void setInstalledAt(LocalDateTime installedAt) {
+        this.installedAt = installedAt;
+    }
+
+    
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
 }
