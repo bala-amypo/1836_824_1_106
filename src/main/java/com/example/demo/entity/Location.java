b2c8c8c490385247c0833392a11fd2a1c9f3fbad
bala-amypo <bala@amypo.in>
2025-12-18 06:51:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -1,60 +1,77 @@
-package.com.example.demo.entity;
+package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDateTime;
+
 @Entity
-public class Location{
+public class Location {
+
     @Id
-      @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String locationName;
     private String description;
     private String region;
     private LocalDateTime createdAt;
+
     
-    public Long getId(){
+    public Location() {
+    }
+
+   
+    public Location(Long id, String locationName, String description,
+                    String region, LocalDateTime createdAt) {
+        this.id = id;
+        this.locationName = locationName;
+        this.description = description;
+        this.region = region;
+        this.createdAt = createdAt;
+    }
+
+   
+
+    public Long getId() {
         return id;
     }
-    public void setId(Long id){
-        this.id=id;
+
+    public void setId(Long id) {
+        this.id = id;
     }
-    public String  getLocationName(){
+
+    public String getLocationName() {
         return locationName;
     }
 
-    public void setLocationName(String locationName){
-        this.locationName=locationName;
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
     }
-    public String getDescription(){
+
+    public String getDescription() {
         return description;
     }
-    public void setDescription(String description){
-        this.description=description;
+
+    public void setDescription(String description) {
+        this.description = description;
     }
-    public String getRegion(){
+
+    public String getRegion() {
         return region;
     }
-    public void setRegion(String region){
-        this.region=region;
+
+    public void setRegion(String region) {
+        this.region = region;
     }
-    public String getCreatedAt(){
+
+    public LocalDateTime getCreatedAt() {
         return createdAt;
     }
-    public void setCreatedAt(String createdAt){
-        this.createdAt=createdAt;
-    }
-    public Location(Long id, String locationName, String description, String region, String createdAt){
-    this.id=id;
-    this.locationName=locationName;
-    this.description=description;
-    this.region=region;
-    this.createdAt=createdAt;
-}
 
-public Location(){
-    
-}
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
