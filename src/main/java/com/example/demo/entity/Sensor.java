55b860f789a3d6a08e8a29e10491007c774d1ae4
bala-amypo <bala@amypo.in>
2025-12-18 07:44:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/Sensor.java
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -1,79 +1,79 @@
-// package.com.example.demo.model;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// @Entity
-// public class Sensor{
-//     @Id
-//     private Long id;
-//     private String sensorCode;
-//     private String sensorType;
-//     private String location;
-//     private LocalDateTime installedAt;
-//     private Boolean isActive;
+package.com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class Sensor{
+    @Id
+    private Long id;
+    private String sensorCode;
+    private String sensorType;
+    private String location;
+    private LocalDateTime installedAt;
+    private Boolean isActive;
 
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
-
-    
-//     public String getSensorCode() {
-//         return sensorCode;
-//     }
-
-//     public void setSensorCode(String sensorCode) {
-//         this.sensorCode = sensorCode;
-//     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     
-//     public String getSensorType() {
-//         return sensorType;
-//     }
+    public String getSensorCode() {
+        return sensorCode;
+    }
 
-//     public void setSensorType(String sensorType) {
-//         this.sensorType = sensorType;
-//     }
-
-    
-//     public String getLocation() {
-//         return location;
-//     }
-
-//     public void setLocation(String location) {
-//         this.location = location;
-//     }
+    public void setSensorCode(String sensorCode) {
+        this.sensorCode = sensorCode;
+    }
 
     
-//     public LocalDateTime getInstalledAt() {
-//         return installedAt;
-//     }
+    public String getSensorType() {
+        return sensorType;
+    }
 
-//     public void setInstalledAt(LocalDateTime installedAt) {
-//         this.installedAt = installedAt;
-//     }
+    public void setSensorType(String sensorType) {
+        this.sensorType = sensorType;
+    }
 
     
-//     public Boolean getIsActive() {
-//         return isActive;
-//     }
+    public String getLocation() {
+        return location;
+    }
 
-//     public void setIsActive(Boolean isActive) {
-//         this.isActive = isActive;
-//     }
-//     public Sensor(Long id, String sensorCode, String sensorType, String location, LocalDateTime installedAt, Boolean isActive){
-//     this.id=id;
-//     this.sensorCode=sensorCode;
-//     this.sensorType=sensorType;
-//     this.location=location;
-//     this.installedAt=installedAt;
-//     this.isActive=isActive;
-// }
-// }
-// public Sensor(){
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
-// }
+    
+    public LocalDateTime getInstalledAt() {
+        return installedAt;
+    }
+
+    public void setInstalledAt(LocalDateTime installedAt) {
+        this.installedAt = installedAt;
+    }
+
+    
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+    public Sensor(Long id, String sensorCode, String sensorType, String location, LocalDateTime installedAt, Boolean isActive){
+    this.id=id;
+    this.sensorCode=sensorCode;
+    this.sensorType=sensorType;
+    this.location=location;
+    this.installedAt=installedAt;
+    this.isActive=isActive;
+}
+}
+public Sensor(){
+
+}
 
 
