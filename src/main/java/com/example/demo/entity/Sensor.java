57d7c156e717ff8f063348a2802133978dbeb59f
bala-amypo <bala@amypo.in>
2025-12-18 07:49:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/Sensor.java
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -1,9 +1,16 @@
-package.com.example.demo.model;
+package.com.example.demo.entity;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
 @Entity
 public class Sensor{
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String sensorCode;
     private String sensorType;
