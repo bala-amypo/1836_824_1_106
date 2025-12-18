537bb804cc3c8960addf202768582d80f78e6ebf
bala-amypo <bala@amypo.in>
2025-12-18 09:49:41 +0000

    User commit

--- src/main/java/com/example/demo/repository/SensorRepository.java
+++ src/main/java/com/example/demo/repository/SensorRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.example.demo.entity.Sensor;
 
-public interface SensorRepository extends JpaRepository<Sensor,Long>{
-
+@Repository
+public interface SensorRepository extends JpaRepository<Sensor, Long> {
 }
