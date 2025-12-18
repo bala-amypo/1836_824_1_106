51f61685fe5c2821c8a028ab7a6b0ec2611cc593
bala-amypo <bala@amypo.in>
2025-12-18 10:36:16 +0000

    User commit

---
+++ src/main/java/com/example/service/SensorService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+import jakarta.persistence.service;
+import com.example.demo.entity.Sensor;
+@Service
+public interface SensorService{
+    public Sensor createSensor()
+}
