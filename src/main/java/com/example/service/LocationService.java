20392ab3664428dd92e74db66fec9ee9e0c4eb88
bala-amypo <bala@amypo.in>
2025-12-19 03:56:07 +0000

    User commit

--- src/main/java/com/example/service/LocationService.java
+++ src/main/java/com/example/service/LocationService.java
@@ -3,5 +3,7 @@ import jakarta.persistence.service;
 import com.example.demo.entity.Location;
 @Location
 public interface LocationService{
-    public Location
+    public Location createLocation(Location location);
+    public Location getLoacation(Long id)(Throw ResourceNotFoundException);
+    public Location getAllLocations();
 }
