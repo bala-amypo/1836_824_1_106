5763b5064a91be2fa61a406acfca1dbfa935a2f2
bala-amypo <bala@amypo.in>
2025-12-19 03:58:51 +0000

    User commit

--- src/main/java/com/example/service/SensorService.java
+++ src/main/java/com/example/service/SensorService.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.Sensor;
 @Service
 public interface SensorService{
     public Sensor createSensor(Long locationId,Sensor sensor);
-
     public Sensor getSensor(Long id)(Throw ResourceNotFoundException);
     public Sensor getAllSensors();
      
