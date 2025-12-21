1d218be3f4c25664431577a679a20f6adb0437ae
bala-amypo <bala@amypo.in>
2025-12-21 14:53:55 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
@@ -1,16 +1,18 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.Location;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Location;
 import com.example.demo.repository.LocationRepository;
 import com.example.demo.service.LocationService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LocationServiceImpl implements LocationService {
 
     private final LocationRepository locationRepository;
 
-    
     public LocationServiceImpl(LocationRepository locationRepository) {
         this.locationRepository = locationRepository;
     }
@@ -18,5 +20,16 @@ public class LocationServiceImpl implements LocationService {
     @Override
     public Location createLocation(Location location) {
         return locationRepository.save(location);
+    }
+
+    @Override
+    public Location getLocation(Long id) {
+        return locationRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Location not found"));
+    }
+
+    @Override
+    public List<Location> getAllLocations() {
+        return locationRepository.findAll();
     }
 }
