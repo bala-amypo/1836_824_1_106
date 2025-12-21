b2cf54b54eeecfdbd3045163c28dcbaf74ea3ded
bala-amypo <bala@amypo.in>
2025-12-21 12:17:17 +0000

    User commit

--- src/main/java/com/example/service/impl/ComplianceEvaluationImpl.java
+++ src/main/java/com/example/service/impl/ComplianceEvaluationImpl.java
@@ -1,20 +1,22 @@
 package com.example.demo.service.impl;
-import com.example.demo.repository.ComplianceEvaluationRepository;
+
 import com.example.demo.entity.ComplianceEvaluation;
+import com.example.demo.repository.ComplianceEvaluationRepository;
 import com.example.demo.service.ComplianceEvaluationService;
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
-
 
 @Service
-public class ComplianceEvaluationServiceImpl implements ComplianceEvaluationService{
+public class ComplianceEvaluationServiceImpl implements ComplianceEvaluationService {
 
-@Autowired
-ComplianceEvaluationRepository cer;
-public ComplianceEvaluation createComplianceEvaluation(ComplianceEvaluation ce){
+    private final ComplianceEvaluationRepository complianceEvaluationRepository;
 
-    return cer.save(ce);
-}
+    
+    public ComplianceEvaluationServiceImpl(ComplianceEvaluationRepository complianceEvaluationRepository) {
+        this.complianceEvaluationRepository = complianceEvaluationRepository;
+    }
 
-
+    @Override
+    public ComplianceEvaluation createComplianceEvaluation(ComplianceEvaluation complianceEvaluation) {
+        return complianceEvaluationRepository.save(complianceEvaluation);
+    }
 }
