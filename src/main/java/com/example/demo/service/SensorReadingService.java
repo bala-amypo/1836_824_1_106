c2e7fae87b7a4a7a642b3515d0bace69c21a2f16
bala-amypo <bala@amypo.in>
2025-12-21 16:02:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/SensorReadingService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.model.*;
+import java.util.List;
+public interface SensorReadingService {
+    SensorReading submitReading(Long sensorId, SensorReading reading);
+    SensorReading getReading(Long id);
+    List<SensorReading> getReadingsBySensor(Long sensorId);
+}
