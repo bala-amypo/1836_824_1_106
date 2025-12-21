ca44d5a43d3325f6bec4c66b9813b886c0b1ac9f
bala-amypo <bala@amypo.in>
2025-12-21 14:56:55 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ComplianceThresholdServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ComplianceThresholdServiceImpl.java
@@ -1,22 +1,41 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.ComplianceThreshold;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.ComplianceThreshold;
 import com.example.demo.repository.ComplianceThresholdRepository;
 import com.example.demo.service.ComplianceThresholdService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ComplianceThresholdServiceImpl implements ComplianceThresholdService {
 
-    private final ComplianceThresholdRepository complianceThresholdRepository;
+    private final ComplianceThresholdRepository repository;
 
-    
-    public ComplianceThresholdServiceImpl(ComplianceThresholdRepository complianceThresholdRepository) {
-        this.complianceThresholdRepository = complianceThresholdRepository;
+    public ComplianceThresholdServiceImpl(ComplianceThresholdRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public ComplianceThreshold createComplianceThreshold(ComplianceThreshold complianceThreshold) {
-        return complianceThresholdRepository.save(complianceThreshold);
+    public ComplianceThreshold createThreshold(ComplianceThreshold threshold) {
+        return repository.save(threshold);
+    }
+
+    @Override
+    public ComplianceThreshold getThreshold(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Threshold not found"));
+    }
+
+    @Override
+    public ComplianceThreshold getThresholdBySensorType(String sensorType) {
+        return repository.findBySensorType(sensorType)
+                .orElseThrow(() -> new ResourceNotFoundException("Threshold not found for sensor type"));
+    }
+
+    @Override
+    public List<ComplianceThreshold> getAllThresholds() {
+        return repository.findAll();
     }
 }
