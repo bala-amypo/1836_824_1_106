e0e77b2f357c9d5e3e0b1ee00291923ea1be4530
bala-amypo <bala@amypo.in>
2025-12-26 10:22:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/ComplianceThreshold.java
+++ src/main/java/com/example/demo/entity/ComplianceThreshold.java
@@ -14,6 +14,15 @@ public class ComplianceThreshold {
     private Double minValue;
     private Double maxValue;
 
+    // id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     // setters
     public void setSensorType(String sensorType) {
         this.sensorType = sensorType;
